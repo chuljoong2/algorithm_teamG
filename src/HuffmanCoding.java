1e1cc4114f6297da7d258b9c152a4b0c7a98d88a
윤철중 <cjfwnd123zz@gmail.com>
2022-04-10 17:45:22 +0900

    우선순위 큐를 생성하고 해쉬 맵에 있는 데이터를 삽입

--- src/HuffmanCoding.java
+++ src/HuffmanCoding.java
@@ -1,7 +1,23 @@
 import java.util.*;
 import java.io.*;
 
+class Node {
+    public char ch;
+    public int freq;
+    public Node left, right;
+}
+
+class FrequencyComparator implements Comparator<Node>{
+    public int compare(Node a, Node b){
+        int freqA = a.freq;
+        int freqB = b.freq;
+        return freqA - freqB;
+    }
+}
+
 public class HuffmanCoding {
+    public static PriorityQueue<Node> queue;
+
     public static void main(String[] args) throws IOException {
         String text = new Scanner(new File("src/alice.txt")).next();
         System.out.println(text);
@@ -17,5 +33,15 @@ public class HuffmanCoding {
         }
         System.out.println(dict);
 
+        queue = new PriorityQueue<Node>(100, new FrequencyComparator());
+        int number = 0;
+        for (Character c: dict.keySet()){
+            Node temp = new Node();
+            temp.ch = c;
+            temp.freq = dict.get(c);
+            queue.add(temp);
+            number++;
+        }
+        System.out.println(number);
     }
 }
