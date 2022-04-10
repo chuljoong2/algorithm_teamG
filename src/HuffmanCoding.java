5ad823fcb5f192fafa180e2c8a087239ef514f75
윤철중 <cjfwnd123zz@gmail.com>
2022-04-10 16:00:43 +0900

    txt파일 생성 및 txt파일 읽어오기추가

---
+++ src/HuffmanCoding.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class HuffmanCoding {
+    public static void main(String[] args) throws IOException {
+        String text = new Scanner(new File("src/alice.txt")).next();
+        System.out.println(text);
+    }
+}
