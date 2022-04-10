c1ec55dc3f8fa15e688077b2609ba00bfc051588
윤철중 <cjfwnd123zz@gmail.com>
2022-04-10 17:08:31 +0900

    읽어온 문자열을  문자단위로 해쉬맵에 빈도수 저장

--- src/HuffmanCoding.java
+++ src/HuffmanCoding.java
@@ -5,5 +5,17 @@ public class HuffmanCoding {
     public static void main(String[] args) throws IOException {
         String text = new Scanner(new File("src/alice.txt")).next();
         System.out.println(text);
+
+        HashMap<Character, Integer> dict = new HashMap<Character, Integer>();
+        for (int i = 0; i < text.length(); i++){
+            char temp = text.charAt(i);
+            if (dict.containsKey(temp)) {
+                dict.put(temp, dict.get(temp)+1);
+            } else {
+                dict.put(temp, 1);
+            }
+        }
+        System.out.println(dict);
+
     }
 }
