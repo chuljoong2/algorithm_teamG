7ab132768e059a87853a2c70048efb0febeb2c06
Threkey <101376843+Threkey@users.noreply.github.com>
2022-04-12 19:18:02 +0900

    코드 주석추가

---
+++ Chained Matrix Multiplications.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ChainedMatrixMultiplications {
+    private static int[] d;
+    private static int[][] m;
+    final static int INF = Integer.MAX_VALUE / 2; // 양의 무한대
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        d = new int[n]; // 행의 개수
+        m = new int[n][n]; // 최소 곱셈 횟수
+
+        for(int i = 0; i < n; i++) {
+            d[i] = scanner.nextInt();
+        }
+
+        for (int L = 0; L < n-1; L++){
+            for (int i = 1; i <= n-1-L; i++) {
+                int j = i + L;
+                if (i == j){
+                    m[i][j] = 0;  //행렬의 대각성분 0으로 초기화
+                    continue;
+                }
+                m[i][j] = INF;
+                for (int k = i; k <= j-1; k++){
+                    m[i][j] = Math.min(m[i][j], m[i][k] + m[k+1][j] + d[i-1]*d[k]*d[j]);  //연산값과 기존값중 작은값을 업데이트
+                }
+                System.out.println(m[i][j]);
+            }
+        }
+        System.out.println(m[1][n-1]); // answer
+    }
+}
